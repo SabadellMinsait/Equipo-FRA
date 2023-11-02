2a5fc2965a520c8c14e90e5b8963896d2a06926e
52558 <52558@Aavences09>
2023-11-01 18:27:23 -0600

    AAVM Primer cambio

---
+++ Materias/src/main/resources/Import.sql
@@ -0,0 +1,12 @@
+insert into profesor (nombre,rfc,especialidad) values ('Ricardo Francisco Azcar Muñoz','ZAMRI800101A01','Java');
+insert into profesor (nombre,rfc,especialidad) values ('Francisco Miguel Pérez','PEFM820202F01','Tomcat');
+insert into profesor (nombre,rfc,especialidad) values ('Javier Sanchez Juarez','SAJJ760712A02','Maven');
+insert into profesor (nombre,rfc,especialidad) values ('Eduardo Lopez Hernandez','LOHE840321J88','MySQL');
+
+insert into materia (nombre,resumen) values ('Programación Java','Curso para incializar la programación en Java');
+insert into materia (nombre,resumen) values ('Introducción a Maven','Curso desde cero para uso de maven');
+insert into materia (nombre,resumen) values ('Uso de Tomcat','Curso para mostrar aplicaciones sobre Tomcat');
+insert into materia (nombre,resumen) values ('Aplicaciones MySQL','Curso para generar aplicaciones con MySQL');
+
+
+
